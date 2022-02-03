31fec06da46d8284c3dd43df27f2cb8e48570dc5
allisongracer <allisonmgracer@gmail.com>
2022-02-03 11:57:55 -0800

    general layout/theme complete

--- CalEnviroScreen/app.R
+++ CalEnviroScreen/app.R
@@ -1,18 +1,22 @@
 library(shiny)
 library(tidyverse)
 library(bslib)
+library(shinythemes)
 
 
-ui <- fluidPage(theme = bs_theme(versiion = 5),
+ui <- fluidPage(theme = shinytheme("cerulean"),
 navbarPage(
     "CalEnviroScreen",
     tabPanel("Widget 1",
              sidebarLayout(
                  sidebarPanel("Widgets go here"), # end sidebarPanel
-                 maainPanel("OUTPUT GOES HERE",
+                 mainPanel("OUTPUT GOES HERE",
                             plotOutput(""))
                  ) # end sidebarlayout 
-             ) # end tabpanel 
-    tabPanel("Widget 2"),
+             ), # end tabpanel 
+    tabPanel("Widget 2")
     ) #end navbar
 ) # end ui
+
+
+
