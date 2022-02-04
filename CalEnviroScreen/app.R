f9b5ab3ce6acf2299aa92a61af3ace40e609e131
tgries23 <tgries@ucsb.edu>
2022-02-03 22:17:35 -0800

    Updated theme

--- CalEnviroScreen/app.R
+++ CalEnviroScreen/app.R
@@ -3,20 +3,26 @@ library(tidyverse)
 library(bslib)
 library(shinythemes)
 
+# custom theme
+shiny_theme <- bs_theme(bootswatch = "minty")
 
-ui <- fluidPage(theme = shinytheme("cerulean"),
-navbarPage(
-    "CalEnviroScreen",
+# start shiny app
+ui <- fluidPage(theme = shiny_theme,
+navbarPage("CalEnviroScreen",
     tabPanel("Widget 1",
              sidebarLayout(
                  sidebarPanel("Widgets go here"), # end sidebarPanel
                  mainPanel("OUTPUT GOES HERE",
-                            plotOutput(""))
+                            plotOutput("")) # end mainPanel
                  ) # end sidebarlayout 
              ), # end tabpanel 
     tabPanel("Widget 2")
     ) #end navbar
 ) # end ui
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
 
 
 
