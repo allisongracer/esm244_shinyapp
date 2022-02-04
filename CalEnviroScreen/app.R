1d5d3e208b8a0b4891619f441ba7ba208ae982e7
tgries23 <tgries@ucsb.edu>
2022-02-03 22:38:26 -0800

    got app working - need to populate tabs with placeholder widgets and populate app

--- CalEnviroScreen/app.R
+++ CalEnviroScreen/app.R
@@ -2,6 +2,14 @@ library(shiny)
 library(tidyverse)
 library(bslib)
 library(shinythemes)
+library(here)
+library(readxl)
+library(janitor)
+
+# read in data
+calenviroscreen4 <- read_excel(here("Data", "calenviroscreen40resultsdatadictionary_f_2021.xlsx")) %>%
+  drop_na() %>%
+  clean_names()
 
 # custom theme
 shiny_theme <- bs_theme(bootswatch = "minty")
@@ -9,14 +17,19 @@ shiny_theme <- bs_theme(bootswatch = "minty")
 # start shiny app
 ui <- fluidPage(theme = shiny_theme,
 navbarPage("CalEnviroScreen",
-    tabPanel("Widget 1",
+    tabPanel("Widget 1 - Map",
              sidebarLayout(
                  sidebarPanel("Widgets go here"), # end sidebarPanel
+                  checkboxGroupInput(inputId = "pick_parameter",
+                                     label = "Choose parameter:",
+                                     choices = unique(calenviroscreen4$california_county)
+                                     ) # end checkboxGroupInput
+                 ), # end sidebarLayout
+             
                  mainPanel("OUTPUT GOES HERE",
                             plotOutput("")) # end mainPanel
-                 ) # end sidebarlayout 
              ), # end tabpanel 
-    tabPanel("Widget 2")
+    tabPanel("Widget 2 - Bar Graph")
     ) #end navbar
 ) # end ui
 
