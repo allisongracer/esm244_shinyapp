08106e10bdb3a086d71a1e621eb3e328fd96828b
tgries23 <tgries@ucsb.edu>
2022-02-07 10:15:41 -0800

    updated widget 1 and 2 but the outputs aren't working

--- CalEnviroScreen/app.R
+++ CalEnviroScreen/app.R
@@ -17,36 +17,44 @@ shiny_theme <- bs_theme(bootswatch = "minty")
 # start shiny app
 ui <- fluidPage(theme = shiny_theme,
 navbarPage("CalEnviroScreen",
-    tabPanel("Widget 1 - Map",
+    tabPanel("Pollution Map",
              sidebarLayout(
-                 sidebarPanel("Widgets go here"), # end sidebarPanel
-                  checkboxGroupInput(inputId = "pick_parameter",
-                                     label = "Choose parameter:",
-                                     choices = unique(calenviroscreen4$california_county)
-                                     ) # end checkboxGroupInput
-                 ), # end sidebarLayout
-             
-                 mainPanel("OUTPUT GOES HERE",
-                            plotOutput("")) # end mainPanel
-             ), # end tabpanel 
-    tabPanel("Widget 2 - Bar Graph"),
-    sidebarLayout(
-      sidebarPanel(
-        sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30)
-      ), #end sidebarPanel
-      mainPanel(plotOutput("distPlot")
-                ) # end mainPanel
-    ) # end sidebarLayout
+                sidebarPanel(
+                checkboxGroupInput(inputId = "pick_california_county",
+                                    label = "Choose California County:",
+                                    choices = unique(calenviroscreen4$california_county)
+                 ) # end checkboxGroupInput
+               ), #end sidebarPanel                 
+                  mainPanel("Map of California",
+                            plotOutput("cal_plot"))
+             ) # end sidebarLayout
+    ), # end tabpanel 1
+    tabPanel("Pollution Burden Per Capita",
+              sidebarLayout(
+                sidebarPanel(
+                checkboxGroupInput(inputId = "pick_california_county",
+                                   label = "Choose California County:",
+                                   choices = unique(calenviroscreen4$california_county)
+                                   ) # end checkboxGroupInput
+                ), #end sidebarPanel                 
+                mainPanel("Pollution Burden Per Capita",
+                          plotOutput("cal_plot"))
+                ) # end sidebarLayout
+             ), # end tabpanel 2
     ) #end navbar
 ) # end ui
 
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    hist(x, breaks = bins, col = 'darkgreen', border = 'white')
-  })
-}
+  cal_reactive <- reactive({
+    calenviroscreen4 %>%
+      filter(california_county %in% input$pick_california_county)
+  }) # end output$cal_plot
+  
+    output$cal_plot <- renderPlot(
+    ggplot(data = cal_reactive(), aes(x = ozone, y = haz_waste)) +
+    geom_point(aes(color = california_county))
+    )
+} # end output$cal_plot
 
 shinyApp(ui = ui, server = server)
 
