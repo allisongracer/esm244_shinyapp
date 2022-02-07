4492a27b626c7c084d266ee0ee122bf5bf6b702e
tgries23 <tgries@ucsb.edu>
2022-02-07 10:24:31 -0800

    finished widget 2

--- CalEnviroScreen/app.R
+++ CalEnviroScreen/app.R
@@ -26,35 +26,46 @@ navbarPage("CalEnviroScreen",
                  ) # end checkboxGroupInput
                ), #end sidebarPanel                 
                   mainPanel("Map of California",
-                            plotOutput("cal_plot"))
+                            plotOutput("cal_plot1"))
              ) # end sidebarLayout
     ), # end tabpanel 1
     tabPanel("Pollution Burden Per Capita",
-              sidebarLayout(
-                sidebarPanel(
-                checkboxGroupInput(inputId = "pick_california_county",
-                                   label = "Choose California County:",
-                                   choices = unique(calenviroscreen4$california_county)
-                                   ) # end checkboxGroupInput
-                ), #end sidebarPanel                 
-                mainPanel("Pollution Burden Per Capita",
-                          plotOutput("cal_plot"))
-                ) # end sidebarLayout
-             ), # end tabpanel 2
+             sidebarLayout(
+               sidebarPanel(
+                 checkboxGroupInput(inputId = "pick_california_county",
+                                    label = "Choose California County:",
+                                    choices = unique(calenviroscreen4$california_county)
+                 ) # end checkboxGroupInput
+               ), #end sidebarPanel                 
+               mainPanel("Pollution Burden Per Capita",
+                         plotOutput("cal_plot2"))
+             ) # end sidebarLayout
+    ), # end tabpanel 2
     ) #end navbar
 ) # end ui
 
 server <- function(input, output) {
-  cal_reactive <- reactive({
+  cal_reactive1 <- reactive({
     calenviroscreen4 %>%
       filter(california_county %in% input$pick_california_county)
-  }) # end output$cal_plot
+  }) # end output$cal_plot 1
   
-    output$cal_plot <- renderPlot(
-    ggplot(data = cal_reactive(), aes(x = ozone, y = haz_waste)) +
+  cal_reactive2 <- reactive({
+    calenviroscreen4 %>%
+      filter(california_county %in% input$pick_california_county)
+  }) # end output$cal_plot 2
+  
+    output$cal_plot1 <- renderPlot(
+    ggplot(data = cal_reactive1(), aes(x = ozone, y = haz_waste)) +
     geom_point(aes(color = california_county))
-    )
-} # end output$cal_plot
+    ) # end output$cal_plot1
+
+output$cal_plot2 <- renderPlot(
+  ggplot(data = cal_reactive2(), aes(x = ozone, y = haz_waste)) +
+    geom_point(aes(color = california_county))
+)
+} # end output$cal_plot2
+
 
 shinyApp(ui = ui, server = server)
 
